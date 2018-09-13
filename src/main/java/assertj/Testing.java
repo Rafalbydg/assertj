88c74db24bbe7356917e025c3fea06e2b0ceaee2
Rafalbydg <it.bydg@gmail.com>
2018-09-13 10:42:08 +0200

    first

---
+++ src/main/java/assertj/Testing.java
@@ -0,0 +1,15 @@
+package assertj;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class Testing {
+
+	String abc = "abc";
+	
+	@Test
+	public void stringIsEmpty() throws Exception{
+			assertThat(abc.isEmpty()).isFalse();
+	}
+}
